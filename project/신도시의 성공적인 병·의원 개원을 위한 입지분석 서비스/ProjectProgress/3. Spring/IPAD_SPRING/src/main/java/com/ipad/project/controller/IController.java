1d5cb03cf8b5754b43758f5d963050db375e7b06
IPAD-1 <wlsdlseowk@gmail.com>
2024-02-27 19:59:01 +0900

    spring

---
+++ project/신도시의 성공적인 병·의원 개원을 위한 입지분석 서비스/ProjectProgress/3. Spring/IPAD_SPRING/src/main/java/com/ipad/project/controller/IController.java
@@ -0,0 +1,7 @@
+package com.ipad.project.controller;
+
+import org.springframework.ui.Model;
+
+public interface IController {
+	public String viewRecommand(Model model) ;
+}
---
+++ project/신도시의 성공적인 병·의원 개원을 위한 입지분석 서비스/ProjectProgress/3. Spring/IPAD_SPRING/src/main/java/com/ipad/project/getRegionData/service/IGetRegionDataService.java
@@ -0,0 +1,22 @@
+package com.ipad.project.getRegionData.service;
+
+import java.util.ArrayList;
+
+import com.ipad.project.getRegionData.model.GetRegionDataVO;
+import com.ipad.project.getRegionData.model.PointVO;
+
+public interface IGetRegionDataService {
+	int calEmploymentAvgFee(String adm_cd);
+
+	int calRentFee(String adm_cd, String areaSize);
+
+	ArrayList<Integer> calEmploymentFee(String adm_cd, String seniorEmployeeCount, String juniorEmployeeCount);
+
+	GetRegionDataVO calNetProfit(String adm_cd);
+
+	String getResionCode(String regionName);
+	
+	int calculateSale(String adm_cd);
+	
+	PointVO getWeightPoint();
+}
