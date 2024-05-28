d5b7ecff4b0042ea6754e279e17c2a3048016bd9
Dongyoung-Kwak <147033486+Dongyoung-Kwak@users.noreply.github.com>
2024-05-28 12:47:40 +0900

    [level 1] Title: 폰켓몬, Time: 2.37 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> map = new HashSet<>();
+		for(int i=0 ; i < nums.length; i++) {
+			map.add(nums[i]);
+		}
+		if(nums.length/2 >= map.size()) {
+			answer =  map.size();
+		} else {
+		   answer = nums.length/2;
+		}
+        return answer;
+    }
+}
